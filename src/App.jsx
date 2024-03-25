50d5ee8870bb08f4badc4dee842e706c2c3f9aeb
Sohit Mishra <7836020160s@gmail.com>
2024-03-25 23:07:38 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,12 @@
+import { legacy_createStore } from 'redux'
 import './App.css'
-const SET_VALUE = "SET_VALUE";
 
-
-const formData = (State,action)=>{
-  switch(action.type){
-    case INCREMENT :{
-      return State+1;
-    }
-    default:{
-      return State;
-    }
-  }
-}
-
-const store = legacy_createStore(formData, 0);
-
+const store = legacy_createStore();
 function App() {
-  
-  function handleIncrement() {
-    dispatch({ type: INCREMENT });
-  }
-  
+
   return (
     <>
-      <h1>Redux Assignments</h1>
-      <div className="card">
-        <button onClick={handleIncrement}>
-          count is 
-        </button>
-  
-      </div>
-
+     
     </>
   )
 }
